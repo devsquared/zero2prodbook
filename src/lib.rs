10b57bf8d0e3b91f8c4c877b0d13e04d2d91c2fd
Devin Ward <devin@hey.com>
2023-12-18 04:07:47 +0000

    up to 3.8

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,20 @@
 use std::net::TcpListener;
 
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use actix_web::{App, HttpResponse, HttpServer, web};
 use actix_web::dev::Server;
+use serde::Deserialize;
 
-async fn health_check() -> impl Responder {
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
@@ -11,6 +22,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
         .listen(listener)?
         .run();
