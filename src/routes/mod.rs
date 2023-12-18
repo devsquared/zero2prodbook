2a95555625a8bd633c13a9b0a100af29cb0add1a
Devin Ward <devin@hey.com>
2023-12-18 22:58:03 +0000

    up to 3.8.5.2.2

---
+++ src/routes/mod.rs
@@ -0,0 +1,6 @@
+pub use health_check::*;
+pub use subscriptions::*;
+
+pub(crate) mod health_check;
+pub(crate) mod subscriptions;
+
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,13 @@
 use std::net::TcpListener;
 
+use zero2prod::startup::run;
+
 /// Spawn app spins up the app on a random port to use for integration testing.
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind to a random port.");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
