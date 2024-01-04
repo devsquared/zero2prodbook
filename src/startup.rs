6bafc4bae96314deb6b5cedef3222694527be641
Devin Ward <devin@hey.com>
2024-01-04 22:59:36 +0000

    up to 3.10.1

--- src/startup.rs
+++ src/startup.rs
@@ -2,15 +2,21 @@ use std::net::TcpListener;
 
 use actix_web::{App, HttpServer, web};
 use actix_web::dev::Server;
+use sqlx::{PgConnection, PgPool};
 
 use crate::routes::health_check::health_check;
 use crate::routes::subscriptions::subscribe;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(
+    listener: TcpListener,
+    db_pool: PgPool,
+) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(db_pool);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_pool.clone())
     })
         .listen(listener)?
         .run();
