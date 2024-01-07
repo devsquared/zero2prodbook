ac77b09c92b8976dc12bc3597bd8f1bf936d84ad
Devin Ward <devin@hey.com>
2024-01-07 19:03:28 +0000

    up to 4.5

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::net::TcpListener;
 
+use env_logger::Env;
 use sqlx::PgPool;
 
 use zero2prod::configuration::get_configuration;
@@ -7,6 +8,10 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // initialize logger at default of info or above
+    // this can be overridden with RUST_LOG env var
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
